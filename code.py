d10380c8858930e828bb1f76dbdd447b6cb9c21a
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-06-21 17:30:11 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,3 +2,14 @@
 
  # Import module for text-to-speech synthesis
 import pyttsx3
+
+# Import module for opening web browsers
+import webbrowser
+
+ # Import module for generating random numbers or selecting random items
+ import random 
+ 
+# Import module for speech recognition
+ import speech_recognition as sr  
+ 
+ 
