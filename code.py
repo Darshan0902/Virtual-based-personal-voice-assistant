4c927ab11b82a8204be3510695f4358d8d3ad2af
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-06-21 17:34:03 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,38 @@
 # Importing Libraries for the code
 
- # Import module for text-to-speech synthesis
-import pyttsx3
+# Import module for text-to-speech synthesis
+ import pyttsx3
 
 # Import module for opening web browsers
-import webbrowser
+ import webbrowser
 
- # Import module for generating random numbers or selecting random items
+# Import module for generating random numbers or selecting random items
  import random 
  
 # Import module for speech recognition
  import speech_recognition as sr  
  
+# Import module for accessing Wikipedia articles
+ import wikipedia                                  
+
+ # Import module for working with dates and times
+ import datetime  
  
+ # Import module for querying the Wolfram Alpha computational knowledge engine
+ import wolframalpha                                 
+ 
+ # Import module for interacting with the operating system
+ import os      
+ 
+ # Import module for system-specific parameters and functions
+ import sys   
+ 
+ # Import module for sending emails using Gmail
+ import yagmail                                     
+
+ #Import module for creating games and multimedia applications
+ import pygame   
+ 
+ #Import module for accessing the system keyring services
+ import keyring                                      
+
