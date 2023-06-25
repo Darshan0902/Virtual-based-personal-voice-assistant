e382e38b3e2780cded4ef3c9477267ed3d902500
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-06-25 15:57:45 +0530

    Update code.py

--- code.py
+++ code.py
@@ -7,8 +7,9 @@ import webbrowser
  # Import module for generating random numbers or selecting random items
 import random    
  # Import module for speech recognition
-import speech_recognition as sr                    
-import wikipedia                                    # Import module for accessing Wikipedia articles
+import speech_recognition as sr     
+ # Import module for accessing Wikipedia articles
+import wikipedia                                   
 import datetime                                     # Import module for working with dates and times
 import wolframalpha                                 # Import module for querying the Wolfram Alpha computational knowledge engine
 import os                                           # Import module for interacting with the operating system
